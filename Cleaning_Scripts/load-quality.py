46a8248ffad7adf48a120aa4fb93ba8be3818202
Donald Dinerman <ddinerma@andrew.cmu.edu>
2022-11-13 15:46:14 -0500

    issue with psycopg2 installation

--- Cleaning_Scripts/load-quality.py
+++ Cleaning_Scripts/load-quality.py
@@ -15,10 +15,20 @@ Algo:
 import pandas as pd
 import numpy as np
 import sys
+import psycopg2  # Can't find this module even though I installed itS
+import credentials
 
 
+# Connect to server
+conn = psycopg2.connect(
+    host="sculptor.stat.cmu.edu", dbname=credentials.DB_USER,
+    user=credentials.DB_USER, password=credentials.DB_PASSWORD
+)
+
+# hhs_df = pd.read_csv(sys.argv[1])  # Argument input
 hhs_df = pd.read_csv("Data/HHS/2022-09-23-hhs-data.csv")
 
-# Replace -999999.0 with, np.nan,
-hhs_df.replace(-999999.0, np.nan, inplace=True)
-hhs_df["collection_week"] = pd.to_datetime(hhs_df["collection_week"])
+# Data Cleaning
+hhs_df.replace(-999999.0, np.nan, inplace=True)  # Replace -999999.0 with np.nan
+hhs_df["collection_week"] = pd.to_datetime(hhs_df["collection_week"])  # Make date type
+
