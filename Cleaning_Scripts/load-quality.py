0352b11256a4f3688c87618ed7e5f439097c6689
Donald Dinerman <ddinerma@andrew.cmu.edu>
2022-11-13 11:33:17 -0500

    update pd

--- Cleaning_Scripts/load-quality.py
+++ Cleaning_Scripts/load-quality.py
@@ -4,4 +4,15 @@ into our SQL data tables.
 
 Enter branch: git checkout Donny
 Exit branch: git checkout main
+
+Algo:
+**Create data tables in ssh psql**
+1. Load Data into pandas
+2. Clean data as pandas df
+3. Insert into sql data table
 """
+
+import pandas as pd
+
+
+hhs_df = pd.read_csv("")
