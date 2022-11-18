60514237b315f0c3469ed7c9b22ceb88d19db197
Arielwuhu <99055903+Arielwuhu@users.noreply.github.com>
2022-11-18 11:25:44 -0500

    All rows with None are inserted

--- Cleaning_Scripts/load-quality.py
+++ Cleaning_Scripts/load-quality.py
@@ -5,27 +5,21 @@ into our SQL data tables.
 Branch modification.
 """
 
-"""
-Python script to clean, process, and insert the quality data
-into our SQL data tables.
-
-Branch modification.
-"""
-
 import numpy as np
 import pandas as pd
 import sys
-import datetime
 import psycopg
+
 
 '''1. Read data'''
 quality = pd.read_csv("../Data/Quality/" + sys.argv[2])
 
 
 '''2. Data Preprocessing'''
-# Replace 'Not Available' value to NaN
-info1 = info1.replace('Not Available', np.nan)
-## Insert date column as python date object
+# Replace 'Not Available' and 'NaN' value to None
+quality = df.replace('Not Available', None)
+quality = quality.replace(np.nan, None)
+# Insert date column as python date object
 date = sys.argv[1]
 quality['Rating year'] = date
 
@@ -33,19 +27,18 @@ quality['Rating year'] = date
 '''3. Load data into psql'''
 # Connect to psql server
 conn = psycopg.connect(
-    host="sculptor.stat.cmu.edu", dbname="yicheng6",
-    user="yicheng6", password=""
+    host="sculptor.stat.cmu.edu", dbname="DB_USER",
+    user="DB_USER", password="DB_PASSWORD"
 )
 cur = conn.cursor()
 
 
-'''3.1 Hospital_Info(hospital_pk, name, address, city, state, zip_code, 
+'''3.1 Hospital_Info(hospital_pk, name, address, city, state, zip_code,
 ownership, emergency)'''
 # Create a seperate table containing useful columns
-info_table = info1.loc[:,['Facility ID', 'Facility Name', 'Address', 
-'City',\
-                          'State', 'ZIP Code', 'Hospital Ownership', 
-'Emergency Services']]
+info_table = quality.loc[:, ['Facility ID', 'Facility Name', 'Address',
+                           'City', 'State', 'ZIP Code',
+                           'Hospital Ownership', 'Emergency Services']]
 
 # Change the data type
 info_table["Facility ID"] = info_table["Facility ID"].astype('string')
@@ -54,46 +47,43 @@ info_table["Address"] = info_table["Address"].astype('string')
 info_table["City"] = info_table["City"].astype('string')
 info_table["State"] = info_table["State"].astype('string')
 info_table["ZIP Code"] = info_table["ZIP Code"].astype('string')
-info_table["Hospital Ownership"] = info_table["Hospital 
-Ownership"].astype('string')
-info_table["Emergency Services"] = info_table["Emergency 
-Services"].astype('bool')
+info_table["Hospital Ownership"] = info_table[
+                                   "HospitalOwnership"].astype('string')
+info_table["Emergency Services"] = info_table[
+                                   "EmergencyServices"].astype('bool')
 
 # Container to record insert failed row
-key = ['Facility ID', 'Facility Name', 'Address', 'City',\
-       'State', 'ZIP Code', 'Hospital Ownership', 'Emergency Services']
+key = ['Facility ID', 'Facility Name',
+       'Address', 'City', 'State', 'ZIP Code',
+       'Hospital Ownership', 'Emergency Services']
 df_error = pd.DataFrame(columns=key)
 
 num_rows_inserted = 0
 
 # make a new transaction
 with conn.transaction():
-    
-    for index, row in info_table.iterrows():
+
+    for index, row in quality_table.iterrows():
         try:
-            # make a new SAVEPOINT -- like a save in a video game
+            # make a new SAVEPOINT
             cur.execute("SAVEPOINT save1")
-            with conn.transaction():  
+            with conn.transaction():
                 # now insert columns into the data
                 insert = ("INSERT INTO Hospital_Info "
                           "VALUES (%s, %s, %s, %s, %s, %s, %s, %s)")
                 cur.execute(insert, tuple(row))
         except Exception as e:
-            # if an exception/error happens in this block, Postgres goes 
-back to
-            # the last savepoint upon exiting the `with` block
-            print("insert failed in row " + str(index))
+            # if an exception/error happens in this block, Postgres goes
+            # back to the last savepoint upon exiting the `with` block
+            print("insert failed in row " + str(index) + ":", e)
             df_error = pd.concat([df_error, row])
 
-            # add additional logging, error handling here
         else:
-            # no exception happened, so we continue without reverting the 
-savepoint
             num_rows_inserted += 1
-            
-    print('Inserted ' + str(num_rows_inserted) + ' rows for Hospital_Info 
-table.')
-    df_error.to_csv("Error_row_hospitalinfo.csv", index = False)
+
+    print('Inserted ' + str(num_rows_inserted) + ' rows for '
+          'Rating_Time table.')
+    df_error.to_csv("Error_row_hospitalinfo.csv", index=False)
 
 # now we commit the entire transaction
 conn.commit()
@@ -101,60 +91,43 @@ conn.commit()
 
 '''3.2 Rating(hospital_pk, rating_year, rating)'''
 # Create a seperate table containing useful columns
-rate_table = info1.loc[:,["Facility ID","Hospital overall rating", "Rating 
-year"]]
-rate_table["Facility ID"] = rate_table["Facility ID"].astype('string')
-rate_table["Hospital overall rating"] = rate_table["Hospital overall 
-rating"].astype('Int64')
-rate_table['Rating year'] = pd.to_datetime(rate_table['Rating year'], 
-format="%Y-%m-%d")
+rate_table = quality.loc[:, ["Facility ID",
+                           "Hospital overall rating",
+                           "Ratingyear"]]
 
 # Container to record insert failed row
-key = ["hospital_pk","Hospital overall rating", "Rating year"]
+key = ["hospital_pk",
+       "Hospital overall rating",
+       "Rating year"]
 df_error = pd.DataFrame(columns=key)
 
 num_rows_inserted = 0
 
 # make a new transaction
 with conn.transaction():
-    
+
     for index, row in rate_table.iterrows():
         try:
-            # make a new SAVEPOINT -- like a save in a video game
-            cur.execute("SAVEPOINT save1")
-            with conn.transaction():  
-                # now insert  (hospital_pk, rating_year, rating) into the 
-data
-                # since the rating will update several times a year, we 
-have to keep the latest one on the table
+            # make a new SAVEPOINT
+            cur.execute("SAVEPOINT save2")
+            with conn.transaction():
+                # now insert  (hospital_pk, rating_year, rating) into the data
                 insert = ("INSERT INTO Rating "
-                          "VALUES (%(hospital_pk)s, %(rating)s, 
-%(rating_year)s)"
-                          "ON CONFLICT (hospital_pk) DO UPDATE "
-                          "SET rating = %(rating)s, rating_year = 
-%(rating_year)s")
-                
-                cur.execute(insert, {
-                    "hospital_pk": row['Facility ID'],
-                    "rating": row['Hospital overall rating'],
-                    "rating_year": row['Rating year']
-                })
+                          "VALUES (%s,%s,%s)")
+
+                cur.execute(insert, tuple(row))
         except Exception as e:
-            # if an exception/error happens in this block, Postgres goes 
-back to
-            # the last savepoint upon exiting the `with` block
-            print("insert failed in row " + str(index))
+            # if an exception/error happens in this block, Postgres
+            # goes back to the last savepoint upon exiting the `with` block
+            print("insert failed in row " + str(index) + ":", e)
             df_error = pd.concat([df_error, row])
 
-            # add additional logging, error handling here
         else:
-            # no exception happened, so we continue without reverting the 
-savepoint
             num_rows_inserted += 1
 
-    print('Inserted ' + str(num_rows_inserted) + ' rows for Rating_Time 
-table.')
-    df_error.to_csv("Error_row_ratingtime.csv", index = False)
+    print('Inserted ' + str(num_rows_inserted) + ' rows for '
+          'Rating_Time table.')
+    df_error.to_csv("Error_row_ratingtime.csv", index=False)
 
 # now we commit the entire transaction
 conn.commit()
