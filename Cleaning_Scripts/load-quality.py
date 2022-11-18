16818686ed7e74802bbd164e009ebb96784a53a1
Arielwuhu <99055903+Arielwuhu@users.noreply.github.com>
2022-11-18 17:20:16 -0500

    Update load-quality.py

--- Cleaning_Scripts/load-quality.py
+++ Cleaning_Scripts/load-quality.py
@@ -35,11 +35,11 @@ conn = psycopg.connect(
 cur = conn.cursor()
 
 
-'''3.1 Hospital_Info(hospital_pk, name, address, city, state, zip_code,
-ownership, emergency)'''
+'''3.1 Hospital_Info(hospital_pk, name, address, city, state, zip_code, 
+county, ownership, emergency)'''
 # Create a seperate table containing useful columns
 info_table = quality.loc[:, ['Facility ID', 'Facility Name', 'Address',
-                             'City', 'State', 'ZIP Code',
+                             'City', 'State', 'ZIP Code', 'County Name',
                              'Hospital Ownership', 'Emergency Services']]
 
 # Change the data type
@@ -49,6 +49,7 @@ info_table["Address"] = info_table["Address"].astype('string')
 info_table["City"] = info_table["City"].astype('string')
 info_table["State"] = info_table["State"].astype('string')
 info_table["ZIP Code"] = info_table["ZIP Code"].astype('string')
+info_table["County Name"] = info_table["County Name"].astype('string')
 info_table["Hospital Ownership"] = info_table[
                                    "Hospital Ownership"].astype('string')
 info_table["Emergency Services"] = info_table[
@@ -56,7 +57,7 @@ info_table["Emergency Services"] = info_table[
 
 # Container to record insert failed row
 key = ['Facility ID', 'Facility Name',
-       'Address', 'City', 'State', 'ZIP Code',
+       'Address', 'City', 'State', 'ZIP Code', 'County Name',
        'Hospital Ownership', 'Emergency Services']
 df_error = pd.DataFrame(columns=key)
 
@@ -71,29 +72,32 @@ with conn.transaction():
             # make a new SAVEPOINT
             cur.execute("SAVEPOINT save1")
             with conn.transaction():
-                # now insert  (hospital_pk, rating_year, rating) into the data
+                # now insert the data
                 insert = ("INSERT INTO Hospital_Info "
-                          "VALUES(%(hospital_pk)s, %(Facility Name)s,\
-                                  %(Address)s, %(City)s, %(State)s,\
-                                  %(ZIP Code)s,%(Hospital Ownership)s,\
-                                  %(Emergency Services)s) "
+                          "VALUES(%(hospital_pk)s, %(name)s,\
+                                  %(address)s, %(city)s, %(state)s,\
+                                  %(zip_code)s, %(county)%,\
+                                  %(ownership)s,\
+                                  %(emergency)s) "
                           "ON CONFLICT (hospital_pk) DO UPDATE "
-                          "SET name = %(Facility Name)s,\
-                               Address = %(Address)s,\
-                               City = %(City)s,\
-                               State = %(State)s,\
-                               zip_code = %(ZIP Code)s,\
-                               Ownership = %(Hospital Ownership)s,\
-                               Emergency = %(Emergency Services)s")
+                          "SET name = %(name)s,\
+                               address = %(address)s,\
+                               city = %(city)s,\
+                               state = %(state)s,\
+                               zip_code = %(zip_code)s,\
+                               county = %(county)s,\
+                               ownership = %(ownership)s,\
+                               emergency = %(emergency)s")
                 cur.execute(insert, {
                     "hospital_pk": row['Facility ID'],
-                    "Facility Name": row['Facility Name'],
-                    "Address": row['Address'],
-                    "City": row['City'],
-                    "State": row['State'],
-                    "ZIP Code": row['ZIP Code'],
-                    "Hospital Ownership": row['Hospital Ownership'],
-                    "Emergency Services": row['Emergency Services'],
+                    "name": row['Facility Name'],
+                    "address": row['Address'],
+                    "city": row['City'],
+                    "state": row['State'],
+                    "zip_code": row['ZIP Code'],
+                    "county": row['county'],
+                    "ownership": row['Hospital Ownership'],
+                    "emergency": row['Emergency Services'],
                 })
         except Exception as e:
             # if an exception/error happens in this block, Postgres goes
